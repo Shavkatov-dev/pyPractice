fa17455ce10e5958a76b9bbac1ed4b6f354b8f89
Shavkatov-dev <shavkatovjasurbek@gmail.com>
2026-04-11 16:08:23 +0900

    feat: learn string in Python

--- primitive.py
+++ primitive.py
@@ -1,4 +1,4 @@
-print("=================")
+print("======= number ==========")
 
 # in JAVA, variable is a name of storage location
 # in Python, variable is a named reference!
@@ -11,3 +11,25 @@ print(f'the count: {count} and type: {count_type}')
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+
+print("======= string ==========")
+
+# Methods: upper() lower() title() capitalize() swapcase() isupper() islower() istitle() - not mutable, returns new string
+
+
+course = "AI Python FullStack"
+result3 = type(course)
+print(f"the result 1: {result3}")
+
+result = course.title()  # har bir suz 1nchi harfini katta harf qilb beradi!!!
+print(f"the result 2: {result}")
+
+result = course.upper()
+print(f"the result 3: {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result 4: {result}")
+
+
+print("======= boolean ==========")
